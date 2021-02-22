30b3f2ec380d5c38bbf9251fe09b590b865d0c63
Slav Abent <43032346+SlavAbent@users.noreply.github.com>
2021-02-22 18:49:39 +0500

    Revert "Card info"

--- gulpfile.js
+++ gulpfile.js
@@ -22,9 +22,8 @@ function browsersync() {
 
 function scripts() {
 	return src([ 
-		// 'node_modules/jquery/dist/jquery.min.js', 
+		'node_modules/jquery/dist/jquery.min.js', 
 		'app/js/app.js',
-		'app/js/post.js',
 		])
 	.pipe(concat('app.min.js')) 
 	.pipe(uglify()) 
